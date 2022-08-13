efcff9ea42293d3be5b771d55b2d9303ea710245
mrchristian17 <dcwrath17@gmail.com>
2022-08-13 14:34:07 -0600

    Created ui for one question to be displayed at a time.  Added firebase config details

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    Hello World
-    </div>
-  );
+export default function App() {
+	const questions = [
+		{
+			questionText: 'How many years of experience do you have?',
+			responseOptions: [
+				{ answerText: '0' },
+				{ answerText: '1'},
+				{ answerText: '2'},
+				{ answerText: '3+'},
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showThankYouMessage, setShowScore] = useState(false);
+
+	const handleAnswerOptionClick = () => {
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showThankYouMessage ? (
+				<div className='thankyou-section'>
+					Thank you for your response!
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							{/* <span>Question {currentQuestion + 1}</span>/{questions.length} */}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='response-section'>
+						{questions[currentQuestion].responseOptions.map((responseOption) => (
+							<button onClick={() => handleAnswerOptionClick()}>{responseOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
-
-export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBEq2ixr9QUTm8YEjj19WRpK4MJnTbt83U",
+    authDomain: "setter-central-webapp.firebaseapp.com",
+    projectId: "setter-central-webapp",
+    storageBucket: "setter-central-webapp.appspot.com",
+    messagingSenderId: "82989674970",
+    appId: "1:82989674970:web:d045db723304848ba510e3"
+  };
+
+
+
+getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+export {
+  auth,
+  db
+}
